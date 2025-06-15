3c97ec08e5285fab1dbd1c45a4bab1c372b7ca4e
JanierSoca <janiersoca@gmail.com>
2025-06-15 06:42:02 -0400

    Update GameMessage.java

--- crypta zombie final/Zombie/src/presentación/GameMessage.java
+++ crypta zombie final/Zombie/src/presentación/GameMessage.java
@@ -1,1 +1,20 @@
-﻿
+﻿package presentación;
+
+public class GameMessage {
+    private String text;
+    private long creationTime;
+    private final long LIFETIME = 5000;
+
+    public GameMessage(String text) {
+        this.text = text;
+        this.creationTime = System.currentTimeMillis();
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public boolean isExpired() {
+        return System.currentTimeMillis() - creationTime > LIFETIME;
+    }
+}
