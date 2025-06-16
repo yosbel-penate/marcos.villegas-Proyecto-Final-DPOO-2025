4fdb62a8b79f960942cec45afd3786987f7d3a31
lazaro-b <lpaezpenalver@gmail.com>
2025-06-16 09:02:50 -0400

    Update Monk.java

--- crypta zombie final/Zombie/src/businesslogic/Monk.java
+++ crypta zombie final/Zombie/src/businesslogic/Monk.java
@@ -1,1 +1,32 @@
-﻿
+package businesslogic;
+public class Monk extends AbstractPlayer {
+    public Monk(String name, int x, int y) {
+        super(name, "Monje", x, y);
+    }
+
+    @Override
+    protected void initializeStats() {
+        health = maxHealth = 100;
+        damage = 20;
+        maxMoves = 4;
+        range = 1;
+    }
+
+    @Override
+    public void useSpecial() {
+        if (hasSpecialUsed) {
+            GameLogic.getInstance().addGameMessage(name + " ya usó su habilidad especial.");
+            return;
+        }
+        health = Math.min(maxHealth, health + 40);
+        GameLogic logic = GameLogic.getInstance();
+        logic.addGameMessage(name + " medita y recupera salud (+40 HP).");
+        hasSpecialUsed = true;
+        logic.notifyRepaint();
+    }
+
+    @Override
+    public void useSecondSpecial() {
+        GameLogic.getInstance().addGameMessage(name + " no tiene habilidad secundaria.");
+    }
+}﻿
